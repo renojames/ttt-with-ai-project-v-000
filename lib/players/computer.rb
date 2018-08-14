79e14e3a7cdbcdcd0733f5cdb0c3f440fcf0a846
Reno Sorensen <renojames19@gmail.com>
2018-08-14 04:52:42 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -112,7 +112,7 @@ module Players
     end
 
     def random_move
-      positions = self.game.board.cells.each_index.select{|i| arr[i] == 'x'}
+      positions = self.game.board.cells.each_index.select{|cell| arr[self.game.board.cells] == 'x'}
       binding.pry
       positions.sample
     end
